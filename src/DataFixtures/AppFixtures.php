b8dab292a54cb5ebe38d517a37ad6dc476f6ce71
Nehemia <nehemiarakotondramanantsoa19gmail.com>
2024-10-11 20:54:19 +0300

    Avant la modification de la vue login

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,9 +7,16 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher) {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
@@ -19,9 +26,11 @@ class AppFixtures extends Fixture
         for($i = 0; $i < 5; $i++) {
             $user = new User();
 
+            $user->setEmail($faker->email);
+            $user->setRoles([]);
+            $user->setPassword($this->hasher->hashPassword($user, '0000'));
             $user->setName($faker->lastName);
             $user->setFirstname($faker->firstName);
-            $user->setEmail($faker->email);
             $user->setCreatedAt(new DateTimeImmutable);
             $user->setUpdatedAt(new DateTimeImmutable);
 
